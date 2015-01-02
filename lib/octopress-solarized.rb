b9c4a75cdb7cd4246805e95fd41042acd4aed678
Brandon Mathis <brandon@imathis.com>
2015-01-02 16:07:02 -0600

    Added source url for Octopress Ink metadata

--- lib/octopress-solarized.rb
+++ lib/octopress-solarized.rb
@@ -6,4 +6,5 @@ Octopress::Ink.add_plugin({
   description: "Style code snippets with Ethan Schoonover's Solarized theme (tweaked a bit).",
   path:         File.expand_path(File.join(File.dirname(__FILE__), '../')),
   version:      Octopress::Solarized::VERSION,
+  source_url:  "https://github.com/octopress/solarized"
 })
